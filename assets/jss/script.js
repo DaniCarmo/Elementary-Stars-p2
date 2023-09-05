9ad9a3a525eaf044142a690d1ee0744c880076b5
DaniCarmo <>
2023-09-05 10:20:50 +0000

    add to function for answer buttons display

--- assets/jss/script.js
+++ assets/jss/script.js
@@ -79,8 +79,15 @@ function selectAnswer() {
     }else{
         selectedBtn.classList.add("incorrect");
     }
+    Array.from(answerButton.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    })
+    nextButton.style.display = "block"
 }
 
 function startQuiz() {
-    
+
 }
