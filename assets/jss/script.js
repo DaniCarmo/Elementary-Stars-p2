57e18a6d0edad6d56ee77fa08108c8761aae7276
DaniCarmo <>
2023-09-14 20:44:19 +0000

    edit validateInputs function to submit form

--- assets/jss/script.js
+++ assets/jss/script.js
@@ -2,7 +2,7 @@ const form = document.getElementById("form");
 const userName = document.getElementById("username");
 
 form.addEventListener("submit", e => {
-    /*e.preventDefault();*/
+    e.preventDefault();
 
     validateInputs();
 });
@@ -13,7 +13,7 @@ const setError = (element, message) => {
     inputControl.classList.add("error");
     inputControl.classList.remove("success");
 }
-
+/*
 const setSuccess = element => {
     const inputControl = element.parentElement;
     const errorDisplay = inputControl.querySelector(".error");
@@ -21,12 +21,13 @@ const setSuccess = element => {
     inputControl.classList.add("success");
     inputControl.classList.remove("error");
 };
+*/
 const validateInputs = () => {
     const usernameValue = userName.value.trim();
     if (usernameValue === "") {
         setError(userName, "Please enter a username!");
     } else {
-        setSuccess(userName);
+        form.submit()
     }
 };
 
