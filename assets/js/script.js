e0b9f3d8d93e63b9305bb122ed0cce3a2cdb345f
DaniCarmo <>
2023-09-22 09:46:17 +0000

    remove trailing slash on html elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -139,7 +139,7 @@ let masterQuestions = [
         ]
     }
 ];
-//function to suffle array questions
+//function to shuffle array questions
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1)); // Generate a random index between 0 and i
