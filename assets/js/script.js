4464ee74aa7ef31f3f2cf21ad86252aad7db384d
DaniCarmo <>
2023-09-26 14:01:16 +0000

    function to hide navbar when modal pops up

--- assets/js/script.js
+++ assets/js/script.js
@@ -261,14 +261,25 @@ function selectAnswer(e) {
     nextButton.style.display = "block";
 }
 
+const navbar = document.getElementById('navbar');
+// Function to hide the navbar
+function hideNavbar() {
+    navbar.classList.add('hidden-navbar');
+}
+// Function to show the navbar
+function showNavbar() {
+    navbar.classList.remove('hidden-navbar');
+}
 // Function to open the score popup
 function openPopup() {
     popup.classList.add("open-popup");
+    hideNavbar();
 }
 
 // Function to close the score popup
 function closePopup() {
     popup.classList.remove("open-popup");
+    showNavbar();
 }
 
 // Function to display the final score
